001931c8ca00281202220926c5e67c7e3947c5ba
Shrikant Ramani <shrikantr9024@gmail.com>
2024-02-25 02:03:01 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-7to0qoyfobe46fwl.us.auth0.com"
+      clientId="0A7GfhaZNZp6aU8MpxJ42EzbOiUFc7NC"
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173",
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
---
+++ client/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck />,
+    heading: "Best interest rates on the market",
+    detail:
+      "Discover curated accommodations for an unforgettable stay. Enjoy exclusive deals and instant booking confirmation. Experience seamless travel planning with our user-friendly platform.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Experience the assurance of steady prices, eliminating surprises in your travel budget. With our platform, rest assured that the rate you book is the rate you pay. Say goodbye to unpredictable price hikes and enjoy the stability you deserve. Book confidently, knowing your accommodation cost remains unchanged.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Discover the best prices available anywhere. Enjoy unbeatable deals on accommodations. Find the perfect stay at the most competitive rates. Your search for the ultimate value ends here.",
+  },
+];
+export default data;
