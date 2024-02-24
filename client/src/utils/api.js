42bcc50dca3b33cfbceb2859537d56f19974a792
Shrikant Ramani <shrikantr9024@gmail.com>
2024-02-25 02:15:09 +0530

    api update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://real-estate-mern-backend-eta.vercel.app//api",
+  baseURL: "https://real-estate-mern-backend-eta.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
